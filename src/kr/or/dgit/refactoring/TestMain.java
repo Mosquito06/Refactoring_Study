224c753aeadf64ea116886ee059085ed61db7c89
Mosquito(A) <skykim10908@naver.com>
2017-12-05 16:13:04 +0900

    20171205일 수업

--- src/kr/or/dgit/refactoring/TestMain.java
+++ src/kr/or/dgit/refactoring/TestMain.java
@@ -3,13 +3,12 @@ package kr.or.dgit.refactoring;
 public class TestMain {
 
 	public static void main(String[] args) {
-		Movie mc1 =new Movie("미니언즈", Movie.CHILDRENS);
-		Movie mc2 = new Movie("니모를 찾아서", Movie.CHILDRENS);
-		Movie mr1 = new Movie("럭키", Movie.REGULAR);
-		Movie mr2 = new Movie("곡성", Movie.REGULAR);
-		Movie mn1 = new Movie("범죄도시", Movie.NEW_RELEASE);
-		Movie mn2 = new Movie("토르3", Movie.NEW_RELEASE);
-
+		Movie mc1 =new Movie("니모를 찾아서", Movie.CHILDRENS);
+		Movie mc2 = new Movie("말레피센트", Movie.CHILDRENS);
+		Movie mr1 = new Movie("명량", Movie.REGULAR);
+		Movie mr2 = new Movie("변호사", Movie.REGULAR);
+		Movie mn1 = new Movie("곡성", Movie.NEW_RELEASE);
+		Movie mn2 = new Movie("인디펜던스데이", Movie.NEW_RELEASE);
 		
 		Rental r1 = new Rental(mc1, 3);
 		Rental r2 = new Rental(mc2, 4);
@@ -18,12 +17,12 @@ public class TestMain {
 		Rental r5 = new Rental(mn1, 2);
 		Rental r6 = new Rental(mn2, 3);
 	
-		Customer c = new Customer("배재진");
+		Customer c = new Customer("김주하");
 		c.addRental(r1);
 		c.addRental(r3);
 		c.addRental(r5);
 		
-		Customer c1 = new Customer("김동환");
+		Customer c1 = new Customer("김희영");
 		c1.addRental(r2);
 		c1.addRental(r4);
 		c1.addRental(r6);
